ca69fff4cba568846b5ab1be977d8a5bbbe8b006
Wiktoria <w.stec19@gmail.com>
2024-10-23 13:13:27 +0200

    added spring boot

---
+++ src/main/java/com/joboffers/domain/JobOffersSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.joboffers.domain;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersSpringBootApplication.class, args);
+    }
+}
