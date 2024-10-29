a64763c77ffd78e9f3d3e25a09debef58ac5f6c9
Wiktoria <w.stec19@gmail.com>
2024-10-29 18:40:14 +0100

    added test and endpoint for finding offer by id,now tests are not passing

--- src/main/java/com/joboffers/infrastructure/offer/controller/OfferRestController.java
+++ src/main/java/com/joboffers/infrastructure/offer/controller/OfferRestController.java
@@ -5,6 +5,7 @@ import com.joboffers.domain.offer.dto.OfferResponseDto;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class OfferRestController {
     public ResponseEntity<List<OfferResponseDto>> findAllOffers() {
         List<OfferResponseDto> allOffers = offerFacade.findAllOffers();
         return ResponseEntity.ok(allOffers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OfferResponseDto> findOfferById(@PathVariable String id) {
+        OfferResponseDto offerById = offerFacade.findOfferById(id);
+        return ResponseEntity.ok(offerById);
     }
 }
