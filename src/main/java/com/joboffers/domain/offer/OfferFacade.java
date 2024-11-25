bcb13ba0d49ad1dba33a27dbe9773fd5d1b1383f
Wiktoria <w.stec19@gmail.com>
2024-11-25 16:59:51 +0100

    added redis

--- src/main/java/com/joboffers/domain/offer/OfferFacade.java
+++ src/main/java/com/joboffers/domain/offer/OfferFacade.java
@@ -3,6 +3,7 @@ package com.joboffers.domain.offer;
 import com.joboffers.domain.offer.dto.OfferResponseDto;
 import com.joboffers.domain.offer.dto.OfferRequestDto;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,6 +14,7 @@ public class OfferFacade {
     private final OfferRepository offerRepository;
     private final OfferService offerService;
 
+    @Cacheable(cacheNames = "jobOffers")
     public List<OfferResponseDto> findAllOffers() {
         return offerRepository.findAll()
                 .stream()
